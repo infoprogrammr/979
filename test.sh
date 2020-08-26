1b27fe58db370f3d5611f34666dcbdc9c234e789
infoprogrammr <68546554+infoprogrammr@users.noreply.github.com>
2020-08-26 19:22:12 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,9 +2,12 @@
 a=$(pwd)
 command='java Challenge'
 myVar=`echo $a | sed 's/ *$//g'`
-cd /home/theia
+cd /home/theia 
 echo "Executing the test.."
-:> $myVar/.testcaseop2 &&  java -classpath './json-simple-1.1.1.jar:./' Test  $myVar "$command"> $myVar/.testcaseop2
+
+
+:> $myVar/.testcaseop2 &&  java -classpath './json-simple-1.1.1.jar:./' Test2  $myVar "$command"> $myVar/.testcaseop2
+
 if grep -q 'error: ' $myVar/.testcaseop2; then
    echo "SYNTAXERROR"
 else if grep -q 'Great job' $myVar/.testcaseop2; then
@@ -12,7 +15,7 @@ else if grep -q 'Great job' $myVar/.testcaseop2; then
 else if grep -q 'Uh-oh' $myVar/.testcaseop2; then
    echo "TESTCASEFAILED"
 else
-   echo "UNKNOWNERROR"
+   echo "UNKNOWNERROR"    
 fi
 fi
 fi
